53d1b7177ffcecfcf356bc3f4c901e7722fe98aa
Megan McCarty <megan_mccarty@hotmail.com>
2021-08-05 14:17:16 -0400

    commit tree 31144750d51c395c87329df8c3a0ae706e4f5da9

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,3 +1,23 @@
 class Cohort < ActiveRecord::Base
-  
+    has_many :students
+
+    def add_student(name, age)
+        Student.create(name: name, age: age, cohort_id: self.id)
+    end
+
+    def average_age
+        self.students.average(:age)
+    end
+
+    def total_students
+        self.students.count
+    end
+
+    def self.biggest
+        Student.group(:cohort_id).order("count(cohort_id) desc").first.cohort
+    end
+
+    def self.sort_by_mod
+        Cohort.order(current_mod: :desc)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,38 @@
 puts "Clearing old data..."
-Cohort.destroy_all
-Student.destroy_all
+Cohort.delete_all
+Student.delete_all
 
 # This will create 5 cohorts using a random date
 puts "Seeding cohorts..."
 c1 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
+  name: "nyc-dumbo-web-051721",
   current_mod: rand(1..5)
 )
 c2 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
+  name: "nyc-dumbo-web-041021",
   current_mod: rand(1..5)
 )
 c3 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
+  name: "nyc-dumbo-web-022421",
   current_mod: rand(1..5)
 )
 c4 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
+  name: "nyc-dumbo-web-010321",
   current_mod: rand(1..5)
 )
 c5 = Cohort.create(
-  name: "nyc-dumbo-web-#{Faker::Date.forward(years: 1).strftime("%m%d%Y")}",
+  name: "nyc-dumbo-web-030821",
   current_mod: rand(1..5)
 )
 
 puts "Seeding students..."
 # TODO: create seed data for students
 # check your schema and make sure to create students with all the necessary data
+
+20.times do
+    cohort_id = Cohort.ids.sample
+    age = rand(18..70)
+    Student.create(name: Faker::Name.name, age: age, cohort_id: cohort_id)
+end
 
 puts "Done!"
